83697c9147e080cfe60b73f3d55f0fa9a7dc7d63
Bartek Frątczak <bartek.fratczak@akcept.eu>
2024-02-02 01:52:28 +0100

    Adding SCSS and some basic structure for that.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ["@/assets/scss/main.scss", '~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/scss/abstracts/_variables.scss";',
+        },
+      },
+    },
+  },
+});
