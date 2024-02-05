4e8992e52f3e2ebef5ea7bf7e1b750f955a93d84
Paweł <Pawcix@op.pl>
2024-02-05 20:46:03 +0100

    Added new elements in website

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,29 @@
 <template>
-    <div>
+    <Header />
+
+    <div class="main">
         <h1>Mamy to kurcze :D</h1>
     </div>
+
+    <Footer />
 </template>
 
-<script setup>
+<script>
+import Header from './layouts/Header.vue';
+import Footer from './layouts/Footer.vue';
 
+export default {
+    name: 'index',
+    components: {
+        Header,
+        Footer,
+    }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    .main {
+        height: 100vh;
+    }
+</style>
+
---
+++ pages/layouts/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav class="nav wrapper">
+        <div class="nav__logo">
+            <a href="/"><img src="~/assets/img/logo_httpGURU.png" alt="Logo strony HTTP Guru" class="nav__logo--img"></a>
+        </div>
+        <div class="nav__list">
+            <a href="/" class="nav__list--element">Home</a>
+            <a href="/" class="nav__list--element">O nas</a>
+            <a href="/" class="nav__list--element">Kontakt</a>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/main.scss';
+
+.nav {
+    @include display-justify-align-flex(flex, space-between, center, row);
+    position: sticky;
+    top: 0;
+    padding: 10px 0;
+}
+
+.nav__logo--img {
+    @include width-height(100%, 100px);
+}
+
+.nav__list {
+    @include display-justify-align-flex(flex, center, center, row);
+    gap: 20px;
+}
+
+.nav__list--element {
+    font-weight: 600;
+    transition: all .3s;
+
+    &:hover {
+        color: $primary-color;
+    }
+}
+</style>
