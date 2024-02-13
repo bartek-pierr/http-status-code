48170094d18e8ffe9433c6ce24d6ca3db697552b
Paweł <Pawcix@op.pl>
2024-02-13 17:18:52 +0100

    Creating a preliminary version of the website

--- pages/about.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <h1>about :D</h1>
-    </div>
-</template>
-
-<style lang="scss" scoped></style>
--- pages/contact.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <h1>Contact :D</h1>
-    </div>
-</template>
-
-<style lang="scss" scoped></style>
